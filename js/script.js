7b0b28036bf5b2be7b041e11d2ab79d135e2cd4e
code8d <code8d@gmail.com>
2022-11-03 16:11:31 +0500

    Added registration block, script. Minor fixes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
-const hero = document.querySelector('.hero-block')
+function vh() {
+    const hero = document.querySelector('.hero-block')
+    const loginWrapper = document.querySelector('.login-wrapper')
 
-hero.style.height = `${document.documentElement.clientHeight - 80}px`
-hero.style.minHeight = `820px`
+    const height = (a, b) => {
+        a.style.height = `${document.documentElement.clientHeight - 80}px`
+        a.style.minHeight = `820px`
 
-console.log(document.documentElement.clientHeight)
+        b.style.height = `${document.documentElement.clientHeight - 80}px`
+        b.style.minHeight = `820px`
+    }
+
+    return height(hero, loginWrapper)
+}
+vh()
+
+function showRegister() {
+    const btns = document.querySelectorAll('.header_btn')
+    const registration = document.querySelector('.login-wrapper')
+    const wrapper = document.querySelector('.wrapper')
+
+    btns[2].addEventListener('click', () => {
+
+        registration.classList.toggle('login-show')
+
+        if (registration.classList.contains('login-show')) {
+            wrapper.style.overflow = 'hidden'
+            wrapper.style.maxHeight = '100vh'
+        }   else {
+            wrapper.style.overflow = 'auto'
+            wrapper.style.maxHeight = 'auto'
+        }
+    })
+}
+showRegister()
