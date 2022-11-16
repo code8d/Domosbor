b8107bbbcb85f82c4ee312644a5571e726b70fc9
code8d <code8d@gmail.com>
2022-11-16 13:53:56 +0500

    Fixed 'toggle' script

--- js/toggle.js
+++ js/toggle.js
@@ -2,7 +2,7 @@ const itemsBlock = document.querySelector('.items-block')
 const items = itemsBlock.querySelectorAll('.item')
 
 items.forEach(item => {
-    item.addEventListener('click', () => {
+    item.children[0].addEventListener('click', () => {
         toggle(item.children[0])
     })
 })
@@ -10,13 +10,13 @@ items.forEach(item => {
 function toggle(element) {
     const parent = element.parentNode.parentNode
     const grandParent = parent.parentNode
-    const itemsChlidrens = grandParent.children[0].children
+    const items = grandParent.children[0].children
     const content = element.nextElementSibling
 
-    for (let item of itemsChlidrens) {
+    for (let item of items) {
         if (item === element.parentNode) {
             element.parentNode.classList.toggle('active')
-            content.classList.toggle('active')
+            content.classList.add('active')
         }   else {
             item.children[1].classList.remove('active')
             item.classList.remove('active')
