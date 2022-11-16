578ac06059ad4200be64f502bac60ba809ff93cd
code8d <code8d@gmail.com>
2022-11-16 13:43:03 +0500

    Fixed bug

--- js/toggle.js
+++ js/toggle.js
@@ -2,30 +2,25 @@ const itemsBlock = document.querySelector('.items-block')
 const items = itemsBlock.querySelectorAll('.item')
 
 items.forEach(item => {
-    item.addEventListener('click', (e) => {
-        // if (e.target === item.children[0]) {
-        //     toggle(e)
-        // }
-        toggle(e)
+    item.addEventListener('click', () => {
+        toggle(item.children[0])
     })
 })
 
 function toggle(element) {
-    const target = element.target
-    const parent = target.parentNode.parentNode
+    const parent = element.parentNode.parentNode
     const grandParent = parent.parentNode
     const itemsChlidrens = grandParent.children[0].children
-    const content = target.nextElementSibling
+    const content = element.nextElementSibling
 
     for (let item of itemsChlidrens) {
-        if (item === target.parentNode) {
-            target.parentNode.classList.toggle('active')
+        console.log(item)
+        if (item === element.parentNode) {
+            element.parentNode.classList.toggle('active')
             content.classList.toggle('active')
         }   else {
             item.children[1].classList.remove('active')
             item.classList.remove('active')
         }
     }
-
-    console.log(target.tagName)
 }
